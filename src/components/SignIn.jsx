34af2b261e4b7774af76dc284f24b604036d3cc4
stefanos <sagelastos@gmail.com>
2019-09-05 16:13:14 +0200

    removed unused options

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -94,10 +94,6 @@ class SignIn extends React.Component {
             value={this.state.password}
             onChange={this.handleInputChange}
           />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
           <Button
             type="submit"
             fullWidth
@@ -108,11 +104,6 @@ class SignIn extends React.Component {
             Sign In
           </Button>
           <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
             <Grid item>
               <Link href="/signup" variant="body2">
                 {"Don't have an account? Sign Up"}
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -138,12 +138,6 @@ class SignUp extends React.Component {
                 onChange={this.handleInputChange}
               />
             </Grid>
-            <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="I want to receive inspiration, marketing promotions and updates via email."
-              />
-            </Grid>
           </Grid>
           <Button
             type="submit"
